10c4a43c0ed374715284d3f432535ed929e162ce
yuunhae <susu492@naver.com>
2025-09-23 21:30:36 +0900

    git commit -m "fix: update file name from CalenderComponents to CalendarComponents"

--- src/lib/components/calendar/index.ts
+++ src/lib/components/calendar/index.ts
@@ -1,4 +1,4 @@
-export { default as CalendarComponents } from './CalenderComponents';
+export { default as CalendarComponents } from './CalendarComponents';
 export { default as CalendarHeader } from './CalendarHeader';
 export { default as CalendarCell } from './CalendarCell';
 export { default as WeekdayHeader } from './WeekdayHeader';
