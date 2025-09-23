b6a3d704cd1971647ddf9afd45a665b594cf602f
yuunhae <susu492@naver.com>
2025-09-23 21:21:41 +0900

    feat: enhance calendar accessibility - Add aria-label with formatted date information - Add aria-current attribute for current date indication

--- src/lib/components/calendar/CalendarCell.tsx
+++ src/lib/components/calendar/CalendarCell.tsx
@@ -13,6 +13,8 @@ const CalendarCell = ({
       className={`bg-color-transition flex h-full min-h-62 w-full items-start rounded-8 border border-gray-300 hover:bg-gray-200`}
     >
       <time
+        aria-label={day.kstDate.toString()}
+        aria-current="date"
         dateTime={day.formattedDate}
         className={`body-16-400 w-full ${
           isCurrentMonth ? 'text-gray-700' : 'text-gray-400'
